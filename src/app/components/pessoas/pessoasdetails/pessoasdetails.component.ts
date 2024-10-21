b90064eda5ffaeb4bb7db39be210e75532752605
Willian Bogler <willian.silva@pti.org.br>
2024-10-21 15:11:21 -0300

    add ou alter on list

--- src/app/components/pessoas/pessoasdetails/pessoasdetails.component.ts
+++ src/app/components/pessoas/pessoasdetails/pessoasdetails.component.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Pessoa } from '../../../models/pessoa';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pessoasdetails',
   standalone: true,
-  imports: [],
+  imports: [MdbFormsModule,FormsModule],
   templateUrl: './pessoasdetails.component.html',
   styleUrl: './pessoasdetails.component.scss'
 })
 export class PessoasdetailsComponent {
+
+  pessoa:Pessoa = new Pessoa();
+
+  router = inject(ActivatedRoute)
+  routerGenereic = inject(Router)
+
+  constructor(){
+    let id = this.router.snapshot.params['id']
+    if(id>0){
+      this.findById(id)
+    }
+  }
+  findById(id:number){
+    //busca no back-end
+    this.pessoa.id = id
+    this.pessoa.nome = "Willian"
+    this.pessoa.documento = "dasda"
+    this.pessoa.idade = 30
+  }
+
+  salvar(){
+    if(this.pessoa.id>0){
+      this.routerGenereic.navigate(['admin/pessoas'], {state:{pessoaEditada: this.pessoa}})
+      alert("Editado com sucesso")
+    }else{
+      this.routerGenereic.navigate(['admin/pessoas'], {state:{pessoaNova: this.pessoa}})
+      alert("Pessoa salva")
+    }
+
+  }
 
 
 
--- src/app/components/pessoas/pessoaslist/pessoaslist.component.ts
+++ src/app/components/pessoas/pessoaslist/pessoaslist.component.ts
@@ -13,6 +13,11 @@ import { CommonModule } from '@angular/common';
 export class PessoaslistComponent {
 
   constructor(){
+
+    const navigation = this.router.getCurrentNavigation();
+    const pessoaNova = navigation?.extras.state?.['pessoaNova'];
+    const pessoaEditada = navigation?.extras.state?.['pessoaEditada'];
+
     let pessoa1:Pessoa = new Pessoa()
     pessoa1.id=1
     pessoa1.nome = "willian"
@@ -20,13 +25,13 @@ export class PessoaslistComponent {
     pessoa1.documento= "asb"
 
     let pessoa2:Pessoa = new Pessoa()
-    pessoa2.id=1
+    pessoa2.id=2
     pessoa2.nome = "Bogler"
     pessoa2.idade = 32
     pessoa2.documento= "aasdasb"
 
     let pessoa3:Pessoa = new Pessoa()
-    pessoa3.id=1
+    pessoa3.id=3
     pessoa3.nome = "Silva"
     pessoa3.idade = 31
     pessoa3.documento= "asd9898"
@@ -34,6 +39,17 @@ export class PessoaslistComponent {
     this.pessoas.push(pessoa1)
     this.pessoas.push(pessoa2)
     this.pessoas.push(pessoa3)
+
+    if(pessoaEditada){
+      let indice = this.pessoas.findIndex(
+        x => {return x.id == pessoaEditada.id}
+      )
+      this.pessoas[indice] = pessoaEditada
+
+    }else if(pessoaNova){
+      pessoaNova.id = this.pessoas.length + 1
+      this.pessoas.push(pessoaNova)
+    }
   }
 
   tebleHead:string[] = ["id","nome","idade","documento"]
@@ -45,8 +61,15 @@ export class PessoaslistComponent {
     this.router.navigate(['admin/pessoas/new'])
   }
 
-  deletar(){
-    console.log("teste")
+  deletar(pessoa:Pessoa){
+    if(confirm(`Excluir ${pessoa.nome}?`)){
+      let indice = this.pessoas.findIndex(
+        x => {return x.id == pessoa.id}
+      )
+  
+      this.pessoas.splice(indice,1)
+    }
+
   }
 
 }
